cc5cf8dfe766d2fd0586f648de0922b61164b59a
Nanao <example@gmail.com>
2025-01-18 00:54:10 +0900

    refactor: NewsSectionを共通コンポーネントを使用するように更新

--- src/app/components/sections/NewsSection/index.tsx
+++ src/app/components/sections/NewsSection/index.tsx
@@ -1,26 +1,29 @@
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import NewsCard from "../../NewsCard";
 import SectionTitle from "../../SectionTitle";
+import Container from "../../ui/Container";
 import { newsItems } from "../../../data/newsItems";
 
 export default function NewsSection() {
   return (
-    <section id="news" className="container mx-auto px-4">
-      <SectionTitle icon={SparklesIcon}>News & Topics</SectionTitle>
-      <div className="space-y-4 max-w-7xl mx-auto">
-        {newsItems.map((news, i) => (
-          <NewsCard 
-            key={i} 
-            news={news}
-            layout={{
-              aspectRatio: "16:9"
-            }}
-            options={{
-              expandable: true
-            }}
-          />
-        ))}
-      </div>
+    <section id="news">
+      <Container>
+        <SectionTitle icon={SparklesIcon}>News & Topics</SectionTitle>
+        <div className="space-y-4 max-w-7xl mx-auto">
+          {newsItems.map((news, i) => (
+            <NewsCard 
+              key={i} 
+              news={news}
+              layout={{
+                aspectRatio: "16:9"
+              }}
+              options={{
+                expandable: true
+              }}
+            />
+          ))}
+        </div>
+      </Container>
     </section>
   );
 } 
