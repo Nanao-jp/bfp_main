cc86a81daaf48efc85a9312dbab528da0224116e
User <user@example.com>
2025-11-05 15:28:08 +0900

    fix: remove duplicate news item

--- src/app/data/newsItems.ts
+++ src/app/data/newsItems.ts
@@ -35,12 +35,5 @@ export const newsItems: NewsItem[] = [
     summary: "永野芽郁主演の感動のヒューマンドラマ",
     content: `公式HP：<a href="https://www.tv-tokyo.co.jp/haretara/" target="_blank" class="text-[#9EFF00] hover:underline">https://www.tv-tokyo.co.jp/haretara/</a>\n\n永野芽郁 主演\n芳根京子はじめ、萩原利久、江口のりこ、稲垣吾郎 が出演！\n\n現代の看護師が戦時中へタイムスリップ。\n困難な状況を強く生き抜こうとするー　\n"前向きになれる"感動のヒューマンドラマ\n\nテレビ東京開局60周年特別企画「晴れたらいいね」\n2025年1月10日（金）よりPrime Videoで世界配信！`,
     imageUrl: "/images/haretara.webp"
-  },
-  {
-    date: "2025.01.13",
-    title: "テレビ東京開局60周年特別企画ドラマスペシャル『晴れたらいいね』公開！",
-    summary: "永野芽郁主演の感動のヒューマンドラマ",
-    content: `公式HP：<a href="https://www.tv-tokyo.co.jp/haretara/" target="_blank" class="text-[#9EFF00] hover:underline">https://www.tv-tokyo.co.jp/haretara/</a>\n\n永野芽郁 主演\n芳根京子はじめ、萩原利久、江口のりこ、稲垣吾郎 が出演！\n\n現代の看護師が戦時中へタイムスリップ。\n困難な状況を強く生き抜こうとするー　\n"前向きになれる"感動のヒューマンドラマ\n\nテレビ東京開局60周年特別企画「晴れたらいいね」\n2025年1月10日（金）よりPrime Videoで世界配信！`,
-    imageUrl: "/images/haretara.webp"
   }
 ]; 
